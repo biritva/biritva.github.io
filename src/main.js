c37c9c1c8d75dffa1b9ae18fc637cd9eb31ffc85
KaJIe4 <KaJIe4@gmail.com>
2018-05-17 09:35:53 +0500

    filling experiment

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
+var fc=0;
+
 function setup() {
   createCanvas(640,480);
   frameRate(30);
 }
 
 function draw() {
-  if (mouseIsPressed){
-    fill(0);
-  } else {
-    fill(255);
+  if (fc>254){
+    fc=0;
+  }
+  fill(fc);
   }
   ellipse(mouseX,mouseY,80,80)
 }
